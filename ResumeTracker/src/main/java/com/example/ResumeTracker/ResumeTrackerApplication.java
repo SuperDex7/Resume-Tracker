fb68534defd9af650d36014c33283f114a7c2b2a
SuperDex7 <superdexter7@gmail.com>
2024-11-05 16:18:02 -0500

    spring init

---
+++ ResumeTracker/src/main/java/com/example/ResumeTracker/ResumeTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.ResumeTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeTrackerApplication.class, args);
+	}
+
+}
